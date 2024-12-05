b136a1b53665964ac8c9e8fb7efdd5a4d57f3ea2
FabioPilu <fabio.pilu93@gmail.com>
2024-12-05 17:01:28 +0100

    [UPDATE]

--- SOFTWARE/port-scanner/port-scanner-kali/SCANbackend/config.py
+++
@@ -1,3 +0,0 @@
-# Configurazione per MongoDB
-DB_URI = "mongodb+srv://danveglia:YJs5JlMgmcIaokZw@digitaldragons.rvva5.mongodb.net/?retryWrites=true&w=majority&appName=digitaldragons"
-DB_NAME = "digitaldragon_db"
